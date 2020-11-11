39131eae96816b103e18a4deb443ff2a5c688637
root <752087942@qq.com>
2020-11-11 21:18:44 +0800

    update

--- client.cpp
+++ client.cpp
@@ -3,7 +3,13 @@
 #include"network.h"
 
 int main(){
-    int sockfd = connect_to_server(11234, "0.0.0.0");
-
+    int sockfd = connect_to_server(11234, "47.116.133.175");
+    if(sockfd<=0){
+        std::cout<<"connect to server failed."<<std::endl;
+    }
+    else{
+        std::cout<<sockfd<<std::endl;
+        std::cout<<"connect to server successful."<<std::endl;
+    }
     return 0;
 }
